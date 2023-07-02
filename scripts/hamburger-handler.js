e159b5791ef4528351c2187d77bb04dea7a8c0bb
popoxta <popoxta@gmail.com>
2023-07-02 12:08:46 +1200

    Format

--- scripts/hamburger-handler.js
+++ scripts/hamburger-handler.js
@@ -1,4 +1,8 @@
-const toggleElements = [document.querySelector('.hamburger'), document.querySelector('.cover'), ...Array.from(document.querySelectorAll('.nav-list-item > a'))]
+const toggleElements = [
+    document.querySelector('.hamburger'),
+    document.querySelector('.cover'),
+    ...Array.from(document.querySelectorAll('.nav-list-item > a'))]
+
 const hamburger = document.querySelector('.hamburger-container')
 
 const handleShow = () => hamburger.classList.toggle('show-menu')
